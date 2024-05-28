fe00c998683f92a4020dbbaa11bc76ee222e2b56
Bama <jelu0204@gmail.com>
2024-05-28 13:38:48 -0500

    title added to the page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -38,7 +38,7 @@ export const App = () => {
 
     return (
         <>
-
+                <h1>Notes App</h1>
 
             <form onSubmit={handleSubmit} className='form'>
                 <label htmlFor='title'>Title</label>
